47063db8e54287428680e42604949eaa12329848
Pareshroser <59230361+Pareshroser@users.noreply.github.com>
2019-12-28 00:13:33 +0545

    validated the form and set up for submit successfully

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,9 +19,10 @@
                     <form action="{{route('questions.store')}}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="question-title"></label>
-                            <input type="text" class="form-control {{$errors->has('title') ? 'is-invalid' : ''}}"
-                                name="title" id="question-title" placeholder="">
+                            <label for="question-title">Title</label>
+                            <input type="text" value="{{old('title')}}"
+                                class="form-control {{$errors->has('title') ? 'is-invalid' : ''}}" name="title"
+                                id="question-title" placeholder="">
                             @if($errors->has('title'))
                             <div class="invalid-feedback">
                                 <strong>{{$errors->first('title')}}</strong>
@@ -32,7 +33,7 @@
                         <div class="form-group">
                             <label for="question-body">Explain your question</label>
                             <textarea class="form-control {{$errors->has('body') ? 'is-invalid' : ''}}" name="body"
-                                id="question-body" rows="3"></textarea>
+                                id="question-body" rows="3">{{old('body')}}</textarea>
                             @if($errors->has('body'))
                             <div class="invalid-feedback">
                                 <strong>{{$errors->first('body')}}</strong>
